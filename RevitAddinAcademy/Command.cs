7db8271933cb01f7cd97abf6ee40d81b8a6c7f1a
a2joe <joe.hood@gmail.com>
2022-07-07 22:51:22 -0400

    Stuff for spreadsheet data

---
+++ RevitAddinAcademy/Command.cs
@@ -0,0 +1,80 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+
+#endregion
+
+namespace RevitAddinAcademy
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            string excelFile = "";
+
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWb = excelApp.Workbooks.Open(excelFile);
+            Excel.Worksheet excelWs = excelWb.Worksheets.Item[1];
+
+            Excel.Range excelRng = excelWs.UsedRange;
+            int rowCount = excelRng.Rows.Count;
+
+            // do some stuff in Excel FIXME: 
+            List<string[]> dataList = new List<string[]>();
+            for(int i = 1; i < rowCount; i++)
+            {
+                Excel.Range cell1 = excelWs.Cells[i, 1];
+                Excel.Range cell2 = excelWs.Cells[i, 2];
+
+                string data1 = cell1.Value.ToString();
+                string data2 = cell2.Value.ToString();
+
+                string[] dataArray = new string[2];
+                dataArray[0] = data1;
+                dataArray[1] = data2;
+
+                dataList.Add(dataArray);
+            }
+
+            using(Transaction t = new Transaction(doc))
+            {
+                t.Start("Create some Revit stuff");
+                Level curLevel = Level.Create(doc, 100);
+
+                FilteredElementCollector collector = new FilteredElementCollector(doc);
+                collector.OfCategory(BuiltInCategory.OST_TitleBlocks);
+                collector.WhereElementIsElementType();
+
+                ViewSheet curSheet = ViewSheet.Create(doc, collector.FirstElementId());
+                curSheet.SheetNumber = "A10101010";
+                curSheet.Name = "New Sheet";
+
+                t.Commit();
+            }
+
+            
+
+            excelWb.Close();
+            excelApp.Quit();
+
+
+            return Result.Succeeded;
+        }
+    }
+}
--- RevitAddinAcademy/Properties/AssemblyInfo.cs
+++ RevitAddinAcademy/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RevitAddinAcademy")]
 [assembly: AssemblyDescription("Revit Add-In Description for RevitAddinAcademy")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autodesk Inc.")]
+[assembly: AssemblyCompany("Quinn Evans")]
 [assembly: AssemblyProduct("RevitAddinAcademy Revit C# .NET Add-In")]
-[assembly: AssemblyCopyright("Copyright (C) 2020 by Jeremy Tammik, Autodesk Inc.")]
+[assembly: AssemblyCopyright("Copyright (C) 2020 by Joseph Hood, Quinn Evans")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("321044f7-b0b2-4b1c-af18-e71a19252be0")]
+[assembly: Guid("52BE9DEB-F7EE-49BB-8A70-D5694064AEED")]
 
 // Version information for an assembly consists of the following four values:
 //
